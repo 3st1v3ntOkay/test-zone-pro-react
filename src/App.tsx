962448f8d6e0b2c969fd106970d3cc5fccfd3723
Esteban <wiwiwixd732@gmail.com>
2023-12-29 15:39:02 -0800

    feat: new route still under testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 
 import { Navbar } from "./components";
-import { Home, Contact, About } from "./routes";
+import { Home, Contact, About, ContextPage } from "./routes";
 
 export default function App() {
   const location = useLocation();
@@ -14,8 +14,9 @@ export default function App() {
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
           <Route index element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="use-context" element={<ContextPage />} />
         </Routes>
       </AnimatePresence>
     </>
